ac58a2454fe0ef3f14a643b812be5d1279d442a7
Vitor <57811481+vitorrov@users.noreply.github.com>
2022-11-27 18:49:27 -0300

    refactor: add ending date to auction

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -8,12 +8,15 @@ const dynamodb = new AWS.DynamoDB.DocumentClient();
 async function createAuction(event, context) {
   const body = event.body;
   const now = new Date();
+  const endDate = new Date();
+  endDate.setHours(now.getHours() + 1);
 
   const auction = {
     id: uuid(),
     title: body.title,
     status: 'OPEN',
     createdAt: now.toISOString(),
+    endingAt: endDate.toISOString(),
     highestBid: {
       amount: 0,
     }
