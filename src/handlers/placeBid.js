f5a46af3f1ab2865f16017330a74918b903c7ff0
Vitor <57811481+vitorrov@users.noreply.github.com>
2022-11-27 23:29:48 -0300

    refactor: new verifications to place bid

--- src/handlers/placeBid.js
+++ src/handlers/placeBid.js
@@ -10,21 +10,29 @@ const dynamodb = new AWS.DynamoDB.DocumentClient();
 async function placeBid(event, context) {
   const { id } = event.pathParameters;
   const { amount } = event.body;
+  const { email } = event.requestContext.authorizer;
 
   const auction = await getAuction(id);
 
-  if (auction.status === 'CLOSED') throw new createError.Forbidden('You cannot bid on closed auctions!');
+  if (auction.status === 'CLOSED')
+    throw new createError.Forbidden('You cannot bid on closed auctions!');
 
-  if (amount <= auction.highestBid.amount) {
+  if (amount <= auction.highestBid.amount)
     throw new createError.Forbidden(`Your bid must be higher than ${auction.highestBid.amount}!`);
-  }
+
+  if (email === auction.highestBid.bidder)
+    throw new createError.Conflict('Your bid is already the highest one!');
+
+  if (email === auction.seller)
+    throw new createError.Forbidden('You can not place a bid on your own auction!');
 
   const params = {
     TableName: process.env.AUCTIONS_TABLE_NAME,
     Key: { id },
-    UpdateExpression: 'set highestBid.amount = :amount',
+    UpdateExpression: 'set highestBid.amount = :amount, highestBid.bidder = :bidder',
     ExpressionAttributeValues: {
       ':amount': amount,
+      ':bidder': email,
     },
     ReturnValues: 'ALL_NEW',
   };
@@ -54,5 +62,3 @@ export const handler = commonMiddleware(placeBid).use(
     },
   })
 );
-
-
