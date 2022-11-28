189bfcbc1dcbf3e09f7661292ab9839be62b67bf
Vitor <57811481+vitorrov@users.noreply.github.com>
2022-11-28 18:06:06 -0300

    feat: send message to seller if no bids on item

--- src/lib/closeAuction.js
+++ src/lib/closeAuction.js
@@ -21,6 +21,17 @@ export async function closeAuction(auction) {
   const { title, seller, highestBid } = auction;
   const { amount, bidder } = highestBid;
 
+  if (amount === 0) {
+    await sqs.sendMessage({
+      QueueUrl: process.env.MAIL_QUEUE_URL,
+      MessageBody: JSON.stringify({
+        subject: 'No bids on your auction item',
+        recipient: seller,
+        body: `Your item ${title} did not get any bids. Better luck next time!`,
+      }),
+    }).promise();
+  }
+
   const notifySeller = sqs.sendMessage({
     QueueUrl: process.env.MAIL_QUEUE_URL,
     MessageBody: JSON.stringify({
